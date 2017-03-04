efac7e97ca67cf23e03f273b91c2c3636282915d
warren <warrenronsiek@gmail.com>
2017-03-04 00:55:29 -0800

    actually works now

---
+++ onLaunch.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+sudo yum update
+sudo yum upgrade
+sudo yum install -y build-essential git python-pip libfreetype6-dev libxft-dev libncurses-dev libopenblas-dev gfortran python-matplotlib libblas-dev liblapack-dev libatlas-base-dev python-dev python-pydot linux-headers-generic linux-image-extra-virtual unzip python-numpy swig python-pandas python-sklearn unzip wget pkg-config zip g++ zlib1g-dev
+sudo pip install -U pip
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1410/x86_64/cuda-repo-ubuntu1410_7.0-28_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1410_7.0-28_amd64.deb
+rm cuda-repo-ubuntu1410_7.0-28_amd64.deb
+sudo yum update
+sudo yum install -y cuda
+tar -zxf cudnn-6.5-linux-x64-v2.tgz && rm cudnn-6.5-linux-x64-v2.tgz
+sudo cp -R cudnn-6.5-linux-x64-v2/lib* /usr/local/cuda/lib64/
+sudo cp cudnn-6.5-linux-x64-v2/cudnn.h /usr/local/cuda/include/
+sudo reboot
+export CUDA_HOME=/usr/local/cuda
+export CUDA_ROOT=/usr/local/cuda
+export PATH=$PATH:$CUDA_ROOT/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo yum update
+# Hack to silently agree license agreement
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo yum install -y oracle-java8-installer
+sudo yum install pkg-config zip g++ zlib1g-dev
+https://github.com/bazelbuild/bazel/releases/download/0.1.4/bazel-0.1.4-installer-linux-x86_64.sh
+chmod +x bazel-0.1.4-installer-linux-x86_64.sh
+./bazel-0.1.4-installer-linux-x86_64.sh --user
+rm bazel-0.1.4-installer-linux-x86_64.sh
+git clone --recurse-submodules https://github.com/tensorflow/tensorflow
+cd tensorflow
+TF_UNOFFICIAL_SETTING=1 ./configure
+Please specify a list of comma-separated Cuda compute capabilities you want to build with.
+You can find the compute capability of your device at: https://developer.nvidia.com/cuda-gpus.
+Please note that each additional compute capability significantly increases your build time and binary size.
+[Default is: "3.5,5.2"]: 3.0
+bazel build -c opt --config=cuda //tensorflow/cc:tutorials_example_trainer
+bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+sudo pip install --upgrade /tmp/tensorflow_pkg/tensorflow-0.6.0-cp27-none-linux_x86_64.whl
