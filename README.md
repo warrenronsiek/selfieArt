298feb39d793fa16fbfb1b63cbd464231c6e2d47
Warren Hamilton Ronsiek <warrenronsiek@gmail.com>
2017-03-04 15:34:33 -0800

    random modifications

---
+++ README.md
@@ -0,0 +1,8 @@
+SelfieArt
+=========
+
+Using tensorflow [image analogies](https://github.com/awentzonline/image-analogies) to turn selfies into Art!
+
+Instructions
+--------
+TODO
--- onLaunch.sh
+++ onLaunch.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
-sudo yum update
-sudo yum upgrade
-sudo yum install -y build-essential git python-pip libfreetype6-dev libxft-dev libncurses-dev libopenblas-dev gfortran python-matplotlib libblas-dev liblapack-dev libatlas-base-dev python-dev python-pydot linux-headers-generic linux-image-extra-virtual unzip python-numpy swig python-pandas python-sklearn unzip wget pkg-config zip g++ zlib1g-dev
+
+
+#Increase volume size
+aws ec2 modify-volume --region us-west-2b --volume-id vol-0fd122f8c4df2f745 --size 30
+
+ssh -i 'selfie-art.pem' ubuntu@54.191.109.187
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y build-essential git python-pip libfreetype6-dev libxft-dev libncurses-dev libopenblas-dev gfortran python-matplotlib libblas-dev liblapack-dev libatlas-base-dev python-dev python-pydot linux-headers-generic linux-image-extra-virtual unzip python-numpy swig python-pandas python-sklearn unzip wget pkg-config zip g++ zlib1g-dev
 sudo pip install -U pip
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1410/x86_64/cuda-repo-ubuntu1410_7.0-28_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1410_7.0-28_amd64.deb
-rm cuda-repo-ubuntu1410_7.0-28_amd64.deb
-sudo yum update
-sudo yum install -y cuda
-tar -zxf cudnn-6.5-linux-x64-v2.tgz && rm cudnn-6.5-linux-x64-v2.tgz
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+rm cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo apt-get update
+sudo apt-get install -y cuda
+
+# scp -i 'selfie-art.pem' ~/PycharmProjects/selfieArt/libcudnn5_5.1.10-1+cuda8.0_ppc64el.deb ubuntu@54.191.109.187:/home/ubuntu
+# https://developer.nvidia.com/rdp/assets/cudnn-65-linux-v2-asset
+sudo dpkg -i  libcudnn5_5.1.10-1+cuda8.0_ppc64el.deb
+rm libcudnn5_5.1.10-1+cuda8.0_ppc64el.deb
 sudo cp -R cudnn-6.5-linux-x64-v2/lib* /usr/local/cuda/lib64/
 sudo cp cudnn-6.5-linux-x64-v2/cudnn.h /usr/local/cuda/include/
 sudo reboot
@@ -17,23 +27,25 @@ export CUDA_ROOT=/usr/local/cuda
 export PATH=$PATH:$CUDA_ROOT/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64
 sudo add-apt-repository -y ppa:webupd8team/java
-sudo yum update
-# Hack to silently agree license agreement
+sudo apt-get update
+
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-sudo yum install -y oracle-java8-installer
-sudo yum install pkg-config zip g++ zlib1g-dev
-https://github.com/bazelbuild/bazel/releases/download/0.1.4/bazel-0.1.4-installer-linux-x86_64.sh
-chmod +x bazel-0.1.4-installer-linux-x86_64.sh
-./bazel-0.1.4-installer-linux-x86_64.sh --user
-rm bazel-0.1.4-installer-linux-x86_64.sh
+sudo apt-get install -y oracle-java8-installer
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+sudo apt-get update && sudo apt-get install bazel
+sudo apt-get upgrade bazel
+
 git clone --recurse-submodules https://github.com/tensorflow/tensorflow
 cd tensorflow
 TF_UNOFFICIAL_SETTING=1 ./configure
-Please specify a list of comma-separated Cuda compute capabilities you want to build with.
-You can find the compute capability of your device at: https://developer.nvidia.com/cuda-gpus.
-Please note that each additional compute capability significantly increases your build time and binary size.
-[Default is: "3.5,5.2"]: 3.0
+
+
 bazel build -c opt --config=cuda //tensorflow/cc:tutorials_example_trainer
 bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
